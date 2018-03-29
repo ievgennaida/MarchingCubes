829d89b36886ce2bd1435e9db43f778ccac9c68f
Ievgen Naida <evgenynaida@gmail.com>
2018-03-30 02:40:31 +0300

    Added initial project structure.

---
+++ MarchingCubes/MarchingCubes.Test/TestGridTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarchingCubes.Test
+{
+    [TestClass]
+    public class TestGridTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+}
